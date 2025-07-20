c7161bbb2ef3915f34e2a2fa9add42026ad863c5
umeh-promise <umehpromise1000@gmail.com>
2025-07-20 13:01:34 +0100

    Update server configuration and add initial tour model with sample data

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-app.use(morgan('dev'));
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('hello from my custom middleware');
   next();
--- server.js
+++ server.js
@@ -1,6 +1,48 @@
+require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
 const app = require('./app');
 
-const port = 3005;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.0 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  price: 399,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`Error ${err}`);
+  });
+
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
