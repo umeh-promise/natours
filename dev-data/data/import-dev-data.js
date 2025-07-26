e9e5f3a06c45161fc2a2c80293242cd3e2ad57bb
umeh-promise <umehpromise1000@gmail.com>
2025-07-26 12:38:58 +0100

    Enhance data import script: add user and review data import functionality; update users.json with new user details.

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ require('dotenv').config({ path: './config.env' });
 const fs = require('fs');
 const mongoose = require('mongoose');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -18,10 +20,16 @@ mongoose
   .then(() => console.log('DB Connection successful!'));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf8')
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded');
   } catch (error) {
     console.log(error);
@@ -32,6 +40,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted');
   } catch (error) {
     console.log(error);
