bf0c9fc27d10e1941b6484fdd3c6a32d68cc5491
umeh-promise <umehpromise1000@gmail.com>
2025-07-27 03:14:00 +0100

    Enable autoIndex for mongoose connection in server configuration and complete API functionality

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+    autoIndex: true,
   })
   .then(() => console.log('DB Connection successful!'));
 
